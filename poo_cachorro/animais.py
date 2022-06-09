ec400b119b6f5bffcad3e1350ad72a8b3e8ddfc1
BernardoPacheco94 <102324672+BernardoPacheco94@users.noreply.github.com>
2022-06-09 20:43:31 -0300

    refatoracao

--- poo_cachorro/animais.py
+++ poo_cachorro/animais.py
@@ -12,14 +12,32 @@ class Cachorro:
     def dormir(self):
         if self.status == 'ACORDADO':
             self.status = 'DORMINDO'
-            print('Agora vou... zzZZZzzzzZZzzZZz!')
+            print('Agora vou... zzZZZzzzzZZzzZZz!\n')
         else:
-            print('zzZZZzzzzZZzzZZz!')
+            print('zzZZZzzzzZZzzZZz!\n')
     
     
     def acordar(self):
         if self.status == 'DORMINDO':
             self.status = 'ACORDADO'
-            print('Acordei!')
+            print('Acordei!\n')
         else:
-            print('já estou acordado!')
+            print('já estou acordado!\n')
+    
+    def comer(self):
+        if self.status == 'ACORDADO':
+            print('HMMMMMM... NHAM NHAM\n')
+        else:
+            print('to não men zzZZZzzzzZZzzZZz!\n')
+            
+    def correr(self):
+        if self.status == 'ACORDADO':
+            print('vruuuuuummmmm\n')
+        else:
+            print('to não men zzZZZzzzzZZzzZZz!\n')
+            
+    def latir(self):
+        if self.status == 'ACORDADO':
+            print('auuuuuuuuuuuuuuuuu uuuu uuuuuu\n')
+        else:
+            print('to não men zzZZZzzzzZZzzZZz!\n')
--- poo_cachorro/main.py
+++ poo_cachorro/main.py
@@ -5,11 +5,24 @@ toto.nome = 'Cleber'
 toto.raca = 'Street-dog'
 toto.tamanho = 'P'
 toto.pelo = 'baixo'
+
 toto.escrever_quem_sou()
 
 toto.dormir()
 print(toto.status)
 toto.dormir()
 toto.acordar()
-toto.escrever_quem_sou()
+print(toto.status)
+toto.dormir()
+toto.acordar()
+toto.comer()
+toto.correr()
+toto.latir()
+toto.dormir()
+toto.correr()
+toto.latir()
+
+
+
+
 
