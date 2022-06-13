b604caf3d182ace822217a6dd5489a31584ce552
BernardoPacheco94 <102324672+BernardoPacheco94@users.noreply.github.com>
2022-06-13 20:49:43 -0300

    Polimorfismo

--- formas geometricas/formas_geometricas.py
+++ formas geometricas/formas_geometricas.py
@@ -5,12 +5,22 @@ class FormaGeometrica:
     cor_borda = None
     espessura_borda = None
     
+    def metodo_geral(self):
+        print('Eu sou uma forma geometrica')
+    
 class Triangulo(FormaGeometrica):
     base = None
     altura = None
     
+    def __str__(self):
+        return f'eu sou um Triangulo de base {self.base} e altura {self.altura}'
+    
     def calcular_area(self):
         return (self.base*self.altura) / 2
+    
+    def metodo_geral(self):#Mesmo nome setado em FormaGeometrica
+        super().metodo_geral()#Esse comando executa a função criada na mãe
+        print('Metodo geral da calsse filha')
     
 class Quadrilatero(FormaGeometrica):
     base = None
@@ -24,26 +34,7 @@ class Circulo(FormaGeometrica):
     
     def calcular_area(self):
         return pi*self.raio**2
+    def __str__(self):
+        return f'eu sou um Círculo de raio {self.raio}'
 
-triangulo_a = Triangulo()
-triangulo_a.cor_fundo = 'vermelho'
-triangulo_a.cor_borda = 'preto'
-triangulo_a.espessura_borda = 3
-triangulo_a.base = 100
-triangulo_a.altura = 150
-
-
-quadrado = Quadrilatero()
-quadrado.cor_fundo = 'transparente'
-quadrado.cor_borda = 'preto'
-quadrado.espessura_borda = 2
-quadrado.base = 8
-quadrado.altura = 16
-
-
-bolinha = Circulo()
-bolinha.cor_borda = 'azul'
-bolinha.cor_fundo = 'branca'
-bolinha.espessura_borda = 3
-bolinha.raio = 10
 
